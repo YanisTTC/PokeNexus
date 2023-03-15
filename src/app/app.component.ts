b018cd5bc316c5af9ba39f1028639b0d8de9a165
YanisTTC <yanis.ttc94@gmail.com>
2023-03-15 15:36:18 +0100

    Creation du jeu (noms en anglais)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from './poke-apiservice.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'PokeNexus';
+export class AppComponent implements OnInit {
+  pokemon: any;
+  guess: string = '';
+  isCorrect: boolean = false;
+
+  constructor(private pokemonService: PokemonService) { }
+
+  ngOnInit(): void {
+    this.getPokemon();
+  }
+
+  getPokemon() {
+    this.pokemonService.getRandomPokemon().subscribe(pokemon => {
+      this.pokemon = pokemon;
+    });
+  }
+
+  checkGuess() {
+    if (this.guess.toLowerCase() === this.pokemon.name.toLowerCase()) {
+      this.isCorrect = true;
+    } else {
+      this.isCorrect = false;
+    }
+  }
+
+  nextPokemon() {
+    this.guess = '';
+    this.isCorrect = false;
+    this.getPokemon();
+  }
 }
+
---
+++ src/app/poke-apiservice.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+interface PokemonData {
+  id: number;
+  name: string;
+  sprites: {
+    front_default: string;
+  };
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+  private apiUrl = 'https://pokeapi.co/api/v2';
+
+  constructor(private http: HttpClient) {}
+
+  getRandomPokemon(): Observable<PokemonData> {
+    const id = Math.floor(Math.random() * 898) + 1;
+    const url = `${this.apiUrl}/pokemon/${id}`;
+    return this.http.get<PokemonData>(url);
+  }
+}
